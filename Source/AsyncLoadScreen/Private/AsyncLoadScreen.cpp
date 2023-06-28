527d3cfcc5b303d13ea13765fc3497985914a93e
Pablo Sanchez <14944505+Manoloon@users.noreply.github.com>
2023-06-28 09:54:09 +0200

    better loading screen.

--- Source/AsyncLoadScreen/Private/AsyncLoadScreen.cpp
+++ Source/AsyncLoadScreen/Private/AsyncLoadScreen.cpp
@@ -2,6 +2,8 @@
 #include "Runtime/MoviePlayer/Public/MoviePlayer.h"
 #include "Widgets/Images/SThrobber.h"
 
+#define LOCTEXT_NAMESPACE "LoadingScreen"
+
 struct FAsyncLoadingScreenBrush : public FSlateDynamicImageBrush, public FGCObject
 {
 	FAsyncLoadingScreenBrush(const FName InTextureName, const FVector2D & InImageSize)
@@ -26,22 +28,23 @@ public:
 
 	void Construct(const FArguments& InArgs)
 	{
-		static const FName LoadingScreenName(TEXT("/Game/UI/loading"));
-		LoadingScreenBrush = MakeShareable(new FAsyncLoadingScreenBrush(LoadingScreenName,FVector2D(1408,792)));
-		static const FName ThrobberImage(TEXT("/Game/UI/Throbber"));
-		ThrobberImageBrush = MakeShareable(new FAsyncLoadingScreenBrush(ThrobberImage,FVector2D(16,16)));
-		FSlateBrush *BGBrush = new FSlateBrush();
-		BGBrush->TintColor= FLinearColor(0.04f,0.04f,0.04f,1.0f);
+		static const FName LoadingScreenAsset(TEXT("/Game/UI/loading"));
+		LoadingScreenBrush = MakeShareable(new FAsyncLoadingScreenBrush(LoadingScreenAsset,FVector2D(1408,792)));
+		static const FName ThrobberAsset(TEXT("/Game/UI/Throbber"));
+		ThrobberImageBrush = MakeShareable(new FAsyncLoadingScreenBrush(ThrobberAsset,FVector2D(16,16)));
+		//FSlateBrush *BackgroundBrush = new FSlateBrush();
+		//BackgroundBrush->TintColor= FLinearColor(0.04f,0.04f,0.04f,1.0f);
+		const FText SideNoteText = LOCTEXT("Sidenote","Prepare your weapons");
 
 		ChildSlot
-			[
+		[
 			SNew(SOverlay)
 			+ SOverlay::Slot()
 			.HAlign(HAlign_Fill)
 			.VAlign(VAlign_Fill)
 			[
-				SNew(SBorder)	
-				.BorderImage(BGBrush)
+				SNew(SImage)	
+				.ColorAndOpacity(FLinearColor(0.04f,0.04f,0.04f,1.0f))
 			]
 			+SOverlay::Slot()
 			.HAlign(HAlign_Center)
@@ -54,18 +57,33 @@ public:
 			.HAlign(HAlign_Fill)
 			.VAlign(VAlign_Fill)
 			[
-				SNew(SVerticalBox)
-				+SVerticalBox::Slot()
+				SNew(SHorizontalBox)
+				+SHorizontalBox::Slot()
+				.FillWidth(2)
 				.VAlign(VAlign_Bottom)
-				.HAlign(HAlign_Right)
-				.Padding(FMargin(10.0f))
 				[
+					SNew(SVerticalBox)
+					+SVerticalBox::Slot()
+					.VAlign(VAlign_Bottom)
+					.HAlign(HAlign_Center)
+					[
+					SNew(STextBlock)
+					.Text(SideNoteText)
+					.ColorAndOpacity(FLinearColor::Green)
+					]
+					+SVerticalBox::Slot()	
+					.VAlign(VAlign_Bottom)
+					.HAlign(HAlign_Right)
+					.Padding(FMargin(10.0f))
+					[
 					SNew(SThrobber)
 					.PieceImage(ThrobberImageBrush.Get())
 					.Animate(SThrobber::Horizontal)
 					.NumPieces(8)
 					.Visibility(this, &SAsyncLoadScreen::GetLoadIndicatorVisibility)
+					]
 				]
+				
 			]
 		];
 	}
@@ -88,11 +106,11 @@ public:
 	void Construct(const FArguments& InArgs)
 	{
 		static const FName LoadingScreenAsset(TEXT("/Game/UI/UELogo"));
-		LoadingScreenBrush = MakeShareable(new FAsyncLoadingScreenBrush(LoadingScreenAsset,FVector2D(1726,337)));
+		LoadingScreenBrush = MakeShareable(new FAsyncLoadingScreenBrush(LoadingScreenAsset,FVector2D(1726,300)));
 		static const FName ThrobberImageAsset(TEXT("/Game/UI/Throbber"));
 		ThrobberImageBrush = MakeShareable(new FAsyncLoadingScreenBrush(ThrobberImageAsset,FVector2D(16,16)));
-		FSlateBrush *BackgroundBrush = new FSlateBrush();
-		BackgroundBrush->TintColor= FLinearColor(0.0f,0.0f,0.0f,1.0f);
+		//FSlateBrush *BackgroundBrush = new FSlateBrush();
+		//BackgroundBrush->TintColor= FLinearColor(0.0f,0.0f,0.0f,1.0f);
 
 		ChildSlot
 			[
@@ -101,8 +119,8 @@ public:
 			.HAlign(HAlign_Fill)
 			.VAlign(VAlign_Fill)
 			[
-				SNew(SBorder)	
-				.BorderImage(BackgroundBrush)
+				SNew(SImage)	
+				.ColorAndOpacity(FLinearColor::Black)
 			]
 			+SOverlay::Slot()
 			.HAlign(HAlign_Center)
@@ -185,5 +203,5 @@ public:
 		GetMoviePlayer()->SetupLoadingScreen(LoadingScreen);
 	}
 };
-
+#undef LOCTEXT_NAMESPACE
 IMPLEMENT_GAME_MODULE(FAsyncLoadScreenModule, AsyncLoadScreen);
